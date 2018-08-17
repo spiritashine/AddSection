f41f6747af411f2f61b132aaaea13181e17c70e4
huj <huj@xingyuanauto.com>
2018-08-17 17:59:39 +0800

    next page

---
+++ app/src/main/java/com/huj/addsection/mail/MailLogActivity.java
@@ -0,0 +1,15 @@
+package com.huj.addsection.mail;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.huj.addsection.R;
+
+public class MailLogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mail_log);
+    }
+}
